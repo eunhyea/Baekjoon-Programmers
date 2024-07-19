8ce9f5c9f5fac16a2a0af4732e3451be72a3a5ee
eunhyea <135845840+eunhyea@users.noreply.github.com>
2024-07-19 20:28:55 +0900

    Update Programmers-CodeReview.py

--- Programmers-CodeReview.py
+++ Programmers-CodeReview.py
@@ -1,2 +1,6 @@
-#120803 lambda 사용하기
+# 120803 lambda 사용하기
 solution = lambda num1, num2 : num1 - num2
+
+# 120807 return 값에도 조건 추가가 가능함
+def solution(num1, num2):
+    return 1 if num1==num2 else -1
