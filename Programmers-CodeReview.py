08c4706d6296e59d6bec92a0b6816e88369b3cde
eunhyea <135845840+eunhyea@users.noreply.github.com>
2024-08-08 00:43:35 +0900

    Update Programmers-CodeReview.py

--- Programmers-CodeReview.py
+++ Programmers-CodeReview.py
@@ -5,7 +5,7 @@ solution = lambda num1, num2 : num1 - num2
 def solution(num1, num2):
     return 1 if num1==num2 else -1
 
-# set 자료형
+# 입문/배열의 유사도 : set 자료형
 # https://thirsty-hosta-2d1.notion.site/set-bb97fef22c954cee86f23f0fce23109b?pvs=4
 # 내코드
 import numpy
@@ -14,3 +14,12 @@ def solution(s1, s2):
 # 교집합 set1 & set2
 def solution(s1, s2):
     return len(set(s1)&set(s2))
+
+# 입문/점의 위치 구하기 : boolean 자료형 응용
+def solution(dot):
+    quad = [(3,2),(4,1)]
+    return quad[dot[0] > 0][dot[1] > 0]
+
+# 입문/피자 나눠먹기(3) : 올림 계산하기
+def solution(slice, n):
+    return ((n - 1) // slice) + 1
