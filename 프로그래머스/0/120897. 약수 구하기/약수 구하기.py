05c0b2cb57a19be670e30b149105c5f11f7c485e
eunhyea <135845840+eunhyea@users.noreply.github.com>
2024-08-08 22:17:15 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -2,13 +2,10 @@ def solution(n):
     answer = []
     divisor = 1
     
-    if n == 1:
-        return [1]
-    else:
-        while divisor <= n/divisor:
+    while divisor <= n/divisor:
 
-            if n%divisor == 0:
-                answer.extend({divisor, n//divisor})
-            
-            divisor += 1
-        return sorted(answer)
+        if n%divisor == 0:
+            answer.extend({divisor, n//divisor})
+
+        divisor += 1
+    return sorted(answer)
