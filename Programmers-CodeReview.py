5973b071f21dc6601bde0c65e279b57949caeeca
eunhyea <135845840+eunhyea@users.noreply.github.com>
2024-08-08 23:44:07 +0900

    Update Programmers-CodeReview.py

--- Programmers-CodeReview.py
+++ Programmers-CodeReview.py
@@ -29,3 +29,8 @@ def solution(rsp):
     win = {'0':'5', '2':'0', '5':'2'}
     answer = [win[chr] for chr in rsp]
     return ''.join(answer)
+
+# 입문/외계행성의 나이 : 딕셔너리 자료형, lambda, map 사용
+def solution(age):
+    change = {'0': 'a', '1': 'b', '2': 'c', '3': 'd', '4': 'e', '5': 'f', '6': 'g', '7': 'h', '8': 'i', '9': 'j'}
+    return ''.join(map(lambda x: change[x], str(age)))
