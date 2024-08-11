80c269ed5d767b551daf400809b4b41b758934ce
eunhyea <135845840+eunhyea@users.noreply.github.com>
2024-08-11 21:05:29 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,13 +1,12 @@
 def solution(array, n):
     
-    # n이 array에 포함될 때
-    if n in array :
+    if n in array:
         return n
     
-    n_close = abs(max([i-n if i-n < 0 else -101 for i in array]))
-    p_close = abs(min([i-n if i-n > 0 else 101 for i in array]))
+    n_close = abs(max([i-n if i-n < 0 else -101 for i in array ]))
+    p_close = abs(min([i-n if i-n > 0 else 101 for i in array ]))
         
-    # n보다 작은 가까운 값
+    # n다 작은 가까운 값
     if n_close <= p_close:
         return n-n_close
 
