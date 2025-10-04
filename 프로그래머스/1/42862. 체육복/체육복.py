6242299372ec2e4f892d01a2245a3a615f20407f
Eunhye Go <135845840+eunhyea@users.noreply.github.com>
2025-10-04 21:13:00 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,12 +1,27 @@
 def solution(n, lost, reserve):
     answer = 0
-    lost_set = set(lost) - set(reserve)
-    reserve_set = set(reserve) - set(lost)
-
-    for i in reserve_set:
-        if i-1 in lost_set:
-            lost_set.remove(i-1)
-        elif i+1 in lost_set:
-            lost_set.remove(i+1)
+    cnt = {i:1 for i in range(1,n+1)}
     
-    return n - len(lost_set)
+    # 중복 제거
+    for i in lost:
+        cnt[i] -= 1
+    for i in reserve:
+        cnt[i] += 1
+    
+    for i in range(1,n+1):
+        if cnt[i]==2:
+            # 큰 수부터 검토
+            if i-1 > 0 and cnt[i-1]==0:
+                cnt[i]-=1
+                cnt[i-1]+=1
+            elif i+1 <= n and cnt[i+1]==0:
+                cnt[i]-=1
+                cnt[i+1]+=1
+            # 이후 작은 수 검토
+
+
+    for v in cnt.values():
+        if v >= 1:
+            answer += 1
+    
+    return answer
