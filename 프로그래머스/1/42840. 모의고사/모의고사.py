884f5cd5d9034a226af8fe54b6d3253e14a20da2
Eunhye Go <135845840+eunhyea@users.noreply.github.com>
2025-10-04 18:00:20 +0900

    [level 1] Title: 모의고사, Time: 3.61 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,23 +1,22 @@
 from collections import Counter
 
 def solution(answers):
-    first = [1,2,3,4,5] * (10000//5)
-    second = [2,1,2,3,2,4,2,5] * (10000//8 + 1)
-    third = [3,3,1,1,2,2,4,4,5,5] * (10000//10)
-    cnt = Counter({1:0,2:0,3:0})
+    first = [1,2,3,4,5]
+    second = [2,1,2,3,2,4,2,5]
+    third = [3,3,1,1,2,2,4,4,5,5]
+    score = [0,0,0]
     answer = []
 
-    for i in range(len(answers)):    
-        if answers[i] == first[i]:
-            cnt[1] += 1
-        if answers[i] == second[i]:
-            cnt[2] += 1
-        if answers[i] == third[i]:
-            cnt[3] += 1
+    for idx, ans in enumerate(answers):    
+        if ans == first[idx%len(first)]:
+            score[0] += 1
+        if ans == second[idx%len(second)]:
+            score[1] += 1
+        if ans == third[idx%len(third)]:
+            score[2] += 1
     
-    max_score = max(cnt.values())
-    for k,v in cnt.items():
-        if v >= max_score:
-            answer.append(k)
-            
+    for idx, s in enumerate(score):
+        if s == max(score):
+            answer.append(idx+1)
+
     return answer
