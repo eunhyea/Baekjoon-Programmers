96e95a032eace655a57395568436b6f46b5e350f
Eunhye Go <135845840+eunhyea@users.noreply.github.com>
2025-10-11 22:14:01 +0900

    [level 2] Title: 전화번호 목록, Time: 99.86 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,7 @@
 def solution(phone_book):
-    phone_book.sort()
+    phone_book=sorted(phone_book)
 
     for i in range(len(phone_book) - 1):
         if phone_book[i+1].startswith(phone_book[i]):
             return False
-    return True       
+    return True
