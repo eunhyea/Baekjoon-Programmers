d4697084743e6a0d6436ce9199947ebc3389d551
Eunhye Go <135845840+eunhyea@users.noreply.github.com>
2025-10-12 03:48:41 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -2,6 +2,6 @@ def solution(numbers, k):
     # k가 1씩 증가할 때 마다 idx는 1에서부터 += 2
     idx = (k*2-1) % len(numbers)
     if idx == 0:
-        # numbers=[1,2,3], k=2와 같은 경우
+        # 예외) numbers=[1,2,3], k=2와 같은 경우
         idx = len(numbers)
     return idx
