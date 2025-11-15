d876d10d54d5dc4b4c416745a76f42bf6193e411
Eunhye Go <135845840+eunhyea@users.noreply.github.com>
2025-11-15 21:44:45 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -0,0 +1,11 @@
+def solution(sides):
+    answer = 0
+    max_side = max(sides)
+    
+    # slides의 원소가 가장 긴 변일 때
+    answer += max_side - (max_side - min(sides))
+    
+    # 나머지 한 변이 가장 긴 변일 때
+    answer += sum(sides) - max_side - 1
+    
+    return answer
